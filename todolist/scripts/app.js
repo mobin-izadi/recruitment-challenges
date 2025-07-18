bcc662ebb3aa7a887e7e3df7f41ff25ddf8f3a62
mobin <mobinizadi.dev@gmail.com>
2025-07-18 12:19:42 +0330

    feat:Add Show today\s date at the top of the page

--- todolist/scripts/app.js
+++ todolist/scripts/app.js
@@ -7,21 +7,33 @@ const newTaskWrapper = document.querySelector('.new-task-wrapper')
 const newTaskSidebarBtn = document.querySelector('.sidebar__btn')
 const newTaskBtn = document.querySelector('.task-btn')
 const newTaskCloseBtn = document.querySelector('.new-task__close-btn')
+const now = new Date()
+const dateElm = document.querySelector('.date')
 
 
 
 // functions
+// The loader clears the screen.
 const removeLoader = () => {
     body.classList.remove('overflow-hidden')
     loaderWrapper.classList.add('hidden')
 }
 
+//Opens and closes the mobile menu.
 const toggleMobileMenu = () => {
     mobileMenuWrapper.classList.toggle('sidebar--show')
 }
 
+// Opens a new task section.
 const toggleNewTaskWrapper = () => {
     newTaskWrapper.classList.toggle('new-task-wrapper--show')
+}
+
+// Show today's date at the top of the page.
+const showDateNow = () => {
+
+    dateElm.innerHTML = nowShamsi
+
 }
 
 
@@ -31,6 +43,8 @@ const toggleNewTaskWrapper = () => {
 // events
 window.addEventListener('load', () => {
     removeLoader()
+    showDateNow()
+
 })
 
 mobileMenuBtn.addEventListener('click', () => {
