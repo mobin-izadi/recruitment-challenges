055e77e8ff60c18b2b32e299bca9d038fd7dd6b9
mobin <mobinizadi.dev@gmail.com>
2025-08-18 14:57:51 +0330

    feat:Add the shopping cart page header section

--- shopping-cart/public/scripts/cart.js
+++ shopping-cart/public/scripts/cart.js
@@ -1,8 +1,46 @@
 import { logged } from "./utils.js";
 
 //--------------------------------------------Variables
+const offerWrapper = document.querySelector('.offer-wrapper')
 
 //--------------------------------------------functions
+// Type the offer text.
+const typeOffer = () => {
+    offerWrapper.innerHTML = ''
+    let offerText = 'کد تخفیف 20 درصدی : mobin'
+    let index = 0
+    let typing = setInterval(() => {
+        offerWrapper.innerHTML = ''
+        let offer = offerText.slice(0, index)
+        offerWrapper.innerHTML = offer
+        index++
+        if (index > offerText.length) {
+            clearInterval(typing)
+            setTimeout(() => {
+                deleteType(offerText)
+            }, 2000);
+
+        }
+    }, 200);
+
+}
+// delete the offer text.
+const deleteType = (text) => {
+    let index = text.length
+    let typing = setInterval(() => {
+        offerWrapper.innerHTML = ''
+        let offer = text.slice(0, index)
+        offerWrapper.innerHTML = offer
+        index--
+        if (index < 0) {
+            clearInterval(typing)
+            setTimeout(() => {
+                typeOffer()
+            }, 1000);
+
+        }
+    }, 200);
+}
 
 //--------------------------------------------events
 window.addEventListener('load', () => {
@@ -10,4 +48,5 @@ window.addEventListener('load', () => {
     if (isLogin === 'false') {
         window.location.href = 'login.html'
     }
+    typeOffer()
 })
